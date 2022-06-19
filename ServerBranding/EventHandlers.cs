af2de2acf529c1af87f2f7f298fc0042165e8fc1
Dylan Morris <dylanmorris330@gmail.com>
2022-06-19 17:23:37 +0100

    initial

---
+++ ServerBranding/EventHandlers.cs
@@ -0,0 +1,35 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Exiled.
+using Mirror;
+using Respawning;
+using Respawning.NamingRules;
+using System.Linq;
+
+namespace ServerBranding
+{
+    internal class EventHandlers
+    {
+        public void OnRoundStarted()
+        {
+            UnitNamingManager unitNamingManager = RespawnManager.Singleton.NamingManager;
+            SyncList<SyncUnit> units = unitNamingManager.AllUnitNames;
+
+            SyncUnit serverUnit = new SyncUnit()
+            {
+                UnitName = Server.Name
+            };
+
+            units.Insert(0, serverUnit);
+        }
+
+        public void OnRoundEnded(RoundEndedEventArgs ev)
+        {
+            UnitNamingManager unitNamingManager = RespawnManager.Singleton.NamingManager;
+            SyncList<SyncUnit> units = unitNamingManager.AllUnitNames;
+
+            SyncUnit serverUnit = units.FirstOrDefault(x => x.UnitName == Server.Name);
+            units.Remove(serverUnit);
+        }
+    }
+}
--- ServerBranding/Plugin.cs
+++ ServerBranding/Plugin.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Exiled.API.Features;
+using Server = Exiled.Events.Handlers.Server;
+using System;
 
 namespace ServerBranding
 {
-    public class Plugin
+    public class Plugin : Plugin<Config>
     {
+        private EventHandlers _events;
+
+        public static Plugin Instance;
+
+        public override string Name { get; } = "ServerBranding";
+        public override string Author { get; } = "Rowpann's Emporium";
+        public override Version Version { get; } = new Version(1, 0, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 2, 0);
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            _events = new EventHandlers();
+            RegisterEvents();
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            UnregisterEvents();
+            _events = null;
+            Instance = null;
+            base.OnDisabled();
+        }
+
+        public void RegisterEvents()
+        {
+            Server.RoundStarted += _events.OnRoundStarted;
+            //Server.RoundEnded += _events.OnRoundEnded;
+        }
+
+        public void UnregisterEvents()
+        {
+            Server.RoundStarted -= _events.OnRoundStarted;
+            //Server.RoundEnded -= _events.OnRoundEnded;
+        }
     }
 }
